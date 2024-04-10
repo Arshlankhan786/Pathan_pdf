6067c5b32409f4be53cda840a72afb64f027da28
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-04-10 09:16:28 +0530

    Update __init__.py

--- ILovePDF/__init__.py
+++ ILovePDF/__init__.py
@@ -6,7 +6,7 @@ file_name = "ILovePDF/__init__.py"
 
 __author__ = "nabilanavab"
 __email__ = "nabilanavab@gmail.com"
-__telegram__ = "telegram.dog/nabilanavab"
+__telegram__ = "telegram.dog/Pathan_botz"
 __copyright__ = "Copyright 2021, nabilanavab"
 
 iLovePDF = '''
