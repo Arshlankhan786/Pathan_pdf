7966abee631ba3859885f24b2ce1ec7e557dc88a
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-04-10 09:29:29 +0530

    Update ban.py

--- ILovePDF/plugins/dm/ban.py
+++ ILovePDF/plugins/dm/ban.py
@@ -51,7 +51,7 @@ async def _banUser(bot, message):
                 return await procs.edit(
                     f"Before Banning {userINFO.mention}.!\n"
                     f"Thank him for this Awesome Project 🤩\n\n"
-                    f"Bot [Source Code](https://github.com/nabilanavab/iLovePDF) 😲"
+                    f"Bot [Source Code](https://telegram.me/pathan_botz) 😲"
                 )
             elif userINFO.id in dm.ADMINS:
                 return await procs.edit(
